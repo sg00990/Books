12241c7834585c533db6c37ef3b0abf1a5299871
Sarah Graddy <graddysa@icloud.com>
2024-02-12 09:17:22 -0500

    added new book

--- app.py
+++ app.py
@@ -229,6 +229,7 @@ def main():
             {"Month": "February", "Title": "Jujutsu Kaisen #11", "Author": "Gege Akutami", "Genre": "Fantasy", "Language": "Japanese", "Start Date": '02-04-2024', "End Date": '02-04-2024', "Rating": 5, "Pages": 192},
             {"Month": "February", "Title": "A Court of Mist and Fury", "Author": "Sarah J. Maas", "Genre": "Fantasy", "Language": "English", "Start Date": '02-02-2024', "End Date": '02-07-2024', "Rating": 5, "Pages": 624},
             {"Month": "February", "Title": "Naruto #1", "Author": "Masashi Kishimoto", "Genre": "Action", "Language": "Japanese", "Start Date": '02-07-2024', "End Date": '02-08-2024', "Rating": 5, "Pages": 187},
+            {"Month": "February", "Title": "A Court of Wings and Ruin", "Author": "Sarah J. Maas", "Genre": "Fantasy", "Language": "English", "Start Date": '02-08-2024', "End Date": '02-11-2024', "Rating": 5, "Pages": 699}
     ]
     feb_books = pd.DataFrame(feb_book_data)
 
@@ -286,7 +287,7 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("Beyond the Story: 10 Year Record of BTS", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** BTS shares personal, behind-the-scenes stories of their journey so far through interviews and more than three years of in-depth coverage by Myeongseok Kang, who has written about K-pop and other Korean pop culture in various media. Presented chronologically in seven chapters from before the debut of BTS to the present, their vivid voices and opinions harmonize to tell a sincere, lively, and deep story. In individual interviews that have been conducted without a camera or makeup, they illuminate their musical journey from multiple angles and discuss its significance.")
                 st.write("**Thoughts:** As a devoted fan of BTS, I eagerly anticipated the release of a book commemorating their 10-year anniversary. Kang Myeong-seok skillfully narrates the storyline, and the inclusion of interviews with the members adds a valuable layer of insight. Overall, I found the book to be an insightful and enjoyable read.")
@@ -315,7 +316,7 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("Jujutsu Kaisen #10", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** In order to regain use of his crippled body, Kokichi Muta, otherwise known as Mechamaru, has been acting as an informant for the cursed spirits. He’s prepared for the betrayal when he’s thrust into a battle to the death against Mahito, but is knowing his enemy enough against a cursed spirit whose powers keep growing exponentially?")
                 st.write('**Thoughts:** Having been captivated by the masterpiece that is season 2 of the anime Jujutsu Kaisen, I was compelled to purchase several volumes while in Japan. This initial volume was a fantastic start to the series, filled with non-stop action from start to finish. However, it did take me a while to read; who knew I\'d have to learn how to say "cursed technique", "curses", and "sorcerers" in Japanese?')
@@ -344,7 +345,7 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("The Exiled Fleet", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** The Sentinels narrowly escaped the collapsing edge of the Divide. They have mustered a few other surviving Sentinels, but with no engines they have no way to leave the edge of the universe before they starve. Adequin Rake has gathered a team to find the materials they'll need to get everyone out.")
                 st.write("**Thoughts:** The Exiled Fleet serves as an amazing sequel to The Last Watch, a novel I read in 2022. Throughout this gripping read, you'll find yourself on the edge of your seat. The characters indulge in witty banter and unravel colossal secrets, all while navigating perilous life-and-death scenarios. I must admit, I did not understand a lot of the terminology about space physics and engineering, but I enjoyed it nonetheless!")
@@ -387,7 +388,7 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("A Court of Thorns and Roses", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** When nineteen-year-old huntress Feyre kills a wolf in the woods, a terrifying creature arrives to demand retribution. Dragged to a treacherous magical land she knows about only from legends, Feyre discovers that her captor is not truly a beast, but one of the lethal, immortal faeries who once ruled her world.")
                 st.write("**Thoughts:** After seeing this book all over TikTok and my local Barnes & Noble, I eventually gave in and bought it. The first two-thirds of the book were a bit slow-paced, likely due to world-building, but the story really picked up momentum towards the end. Hopefully the sequel has more plot.")
@@ -416,7 +417,7 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("Jujutsu Kaisen #11", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** Despite the crowd of civilians and transfigured humans, Satoru Gojo is able to defeat the cursed spirits at Shibuya Station. But it's a trap! The cursed spirits possess a special item that can even seal the all-powerful Gojo! Meanwhile, an unlikely ally suddenly contacts Yuji Itadori, who is on his way to the station!")
                 st.write("**Thoughts:** In Volume 11 of Jujutsu Kaisen, the focus shifts to the other characters in Shibuya. While it may contain less action compared to the last volume, I appreciated the opportunity to delve deeper into various characters' situations and battles.")
@@ -446,7 +447,7 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("A Court of Mist and Fury", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** Feyre has undergone more trials than one human woman can carry in her heart. Though she's now been granted the powers and lifespan of the High Fae, she is haunted by her time Under the Mountain and the terrible deeds she performed to save the lives of Tamlin and his people. As her marriage to Tamlin approaches, Feyre's hollowness and nightmares consume her. She finds herself split into two different people: one who upholds her bargain with Rhysand, High Lord of the feared Night Court, and one who lives out her life in the Spring Court with Tamlin. While Feyre navigates a dark web of politics, passion, and dazzling power, a greater evil looms. She might just be the key to stopping it, but only if she can harness her harrowing gifts, heal her fractured soul, and decide how she wishes to shape her future-and the future of a world in turmoil.")
                 st.write("**Thoughts:** Ah, now I understand the hype surrounding this series! I found myself utterly captivated by this book, devouring about 100 pages each day. It's so, so good! With its intricate plot, themes of betrayal, complex relationships, and gripping depiction of an upcoming war between Fae and humans alike, it's truly a compelling read. Also, love having a strong female lead!")
@@ -476,10 +477,40 @@ def main():
                     st.write(f"**Number of Pages:** {page_result}")
 
                     rating_result = get_rating("Naruto #1", filtered_data)
-                    st.write(f"**My Rating:** {rating_result}/5")
+                    st.write(f"**My Rating:** {rating_result}/5.0")
 
                 st.write("**Summary:** Twelve years ago the Village Hidden in the Leaves was attacked by a fearsome threat. A nine-tailed fox spirit claimed the life of the village leader, the Hokage, and many others. Today, the village is at peace and a troublemaking kid named Naruto is struggling to graduate from Ninja Academy.")
                 st.write("**Thoughts:** After binge-watching the entire series (yes, all 500+ episodes) over two months years ago, I was thrilled to discover this book at a nearby Kinokuniya. It proved to be a much easier read compared to Jujutsu Kaisen.")
+
+        
+        page_result = get_pages("A Court of Wings and Ruin", book_df)
+        author_result = get_author("A Court of Wings and Ruin", book_df)
+        genre_result = get_genre("A Court of Wings and Ruin", book_df)
+
+        if (genre_result in selected_genre or not selected_genre) and (page_result <= pages) and (author_result in selected_author or not selected_author):
+            with st.expander("A Court of Wings and Ruin"):
+                col6, col7 = st.columns(2)
+
+                with col6:
+                    st.image("img/acowar_book.jpg", width=250)
+
+                with col7:
+                    st.write("**Title:** A Court of Wings and Ruin")
+
+                    st.write(f"**Author:** {author_result}")
+
+                    st.write(f"**Genre:** {genre_result}")
+
+                    language_result = get_language("A Court of Wings and Ruin", filtered_data)
+                    st.write(f"**Language:** {language_result}")
+
+                    st.write(f"**Number of Pages:** {page_result}")
+
+                    rating_result = get_rating("A Court of Wings and Ruin", filtered_data)
+                    st.write(f"**My Rating:** {rating_result}/5.0")
+
+                st.write("**Summary:** Feyre has returned to the Spring Court, determined to gather information on Tamlin’s maneuverings and the invading king threatening to bring Prythian to its knees. But to do so she must play a deadly game of deceit-and one slip may spell doom not only for Feyre, but for her world as well. As war bears down upon them all, Feyre must decide who to trust amongst the dazzling and lethal High Lords-and hunt for allies in unexpected places. In this thrilling third book in the #1 New York Times and USA Today bestselling series from Sarah J. Maas, the earth will be painted red as mighty armies grapple for power over the one thing that could destroy them all.")
+                st.write("**Thoughts:** In 'A Court of Wings and Ruin,' war, betrayals, and unexpected alliances dominated the narrative. I found it enjoyable (and sometimes frustrating) to read!")
     
     
 
